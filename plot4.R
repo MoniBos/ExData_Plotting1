d46f69cb3573b012d3579d13dd41db4f1fa2aef2
MoniBos <MoniBos@users.noreply.github.com>
2016-01-24 00:14:57 -0600

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -14,11 +14,11 @@ datetime <- paste(as.Date(sub$Date1), sub$Time)
 sub$Datetime <- as.POSIXct(datetime)
 
 #plot
+png("plot4.png", width=480, height=480)
 par(mfrow=c(2,2), mar=c(4,4,2,1))
 with(sub,{
 	plot(sub$Global_active_power~sub$Datetime, type="l", xlab="",ylab="Global Active Power")
 	plot(sub$Voltage~sub$Datetime, type="l", xlab="datetime",ylab="Voltage")
-
 	plot(sub$Sub_metering_1~sub$Datetime, type="l", xlab="",ylab="Energy sub metering")
 	lines(sub$Sub_metering_2~sub$Datetime, type="l", xlab="",ylab="Energy sub metering",ylim=c(0,30), col="red")
 	lines(sub$Sub_metering_3~sub$Datetime, type="l", xlab="",ylab="Energy sub metering", ylim=c(0,30),col="blue")
@@ -26,3 +26,4 @@ with(sub,{
 	plot(sub$Global_reactive_power~sub$Datetime, type="l", xlab="datetime",ylab="Global_reactive_power")
 	
 })
+dev.off()
